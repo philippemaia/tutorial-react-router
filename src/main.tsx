ab178101e74c3e2f98320bc5013c552479a6e042
Philippe Maia <philippe.m.maia@hotmail.com>
2026-05-07 01:18:12 -0300

    Acessando parâmetros de URL

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import App from './App.tsx'
 import Expenses from './routes/Expenses'
 import Invoices from './routes/Invoices'
 import NotFound from './routes/NotFound/index.tsx'
+import Invoice from './routes/Invoices/Invoice/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} >
         <Route path="expenses" element={<Expenses />} />
-        <Route path="invoices" element={<Invoices />} />
+        <Route path="invoices" element={<Invoices />}>
+          <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
