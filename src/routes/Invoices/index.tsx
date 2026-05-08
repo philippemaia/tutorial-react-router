052a45dd22620ba50a6d2fe2977f7ea92bb3efcd
Philippe Maia <philippe.m.maia@hotmail.com>
2026-05-08 00:36:22 -0300

    Filtrando barra de navegação com parâmetros de busca

--- src/routes/Invoices/index.tsx
+++ src/routes/Invoices/index.tsx
@@ -1,9 +1,11 @@
-import { NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet, useSearchParams } from "react-router-dom";
 import { getInvoices } from "../../data";
-import './styles.css';
+import "./styles.css";
 
 export default function Invoices() {
   const invoices = getInvoices();
+  const [searchParams, setSearchParams] = useSearchParams();
+
   return (
     <div style={{ display: "flex" }}>
       <nav
@@ -12,9 +14,32 @@ export default function Invoices() {
           padding: "1rem",
         }}
       >
-        {invoices.map((invoice) => (
+        <input
+          value={searchParams.get("name") || ""}
+          onChange={(event) => {
+            const name = event.target.value;
+            if (name) {
+              setSearchParams({ name });
+            } else {
+              setSearchParams({});
+            }
+          }}
+        />
+
+        {invoices
+          .filter((invoice) => {
+            const name = searchParams.get("name");
+            if (!name) {
+              return true;
+            }
+            const invoiceName = invoice.name.toLowerCase();
+            return invoiceName.startsWith(name.toLowerCase());
+          })
+          .map((invoice) => (
           <NavLink
-            className={({ isActive }) => isActive ? "dblock nav-red" : "dblock nav-blue"}
+            className={({ isActive }) =>
+              isActive ? "dblock nav-red" : "dblock nav-blue"
+            }
             to={`/invoices/${invoice.number}`}
             key={invoice.number}
           >
